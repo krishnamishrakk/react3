e38dc371e94309b72546c63de9d8208407773fa3
krishnamishrakk <krishm9294@gmail.com>
2022-05-20 23:48:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Input1 from "./component/Input1";
+import Input3 from "./component/Input3";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Input3/>
+      <Input1/>
+      
     </div>
   );
 }
---
+++ src/component/Input3.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './Input1.css';
+
+function Input3() {
+  return (
+    <div className='textform'>
+        <h2>EMPLOYEE FEEDBACK FORM</h2>
+        </div>
+  )
+}
+
+export default Input3
